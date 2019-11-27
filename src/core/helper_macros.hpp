be428f5a3453862a4e20ea6887b168f55e3b886a
ptrNine <sockskilling@gmail.com>
2019-11-27 21:27:27 +0300

    Add smart pointer aliases to DECLARE_SELF_FABRICS

--- src/core/helper_macros.hpp
+++ src/core/helper_macros.hpp
@@ -16,8 +16,6 @@ void FIELD(const decltype(_##FIELD)& value) { _##FIELD = value; }
 
 
 #define DECLARE_SELF_FABRICS(CLASS)  \
-using SharedPtr = std::shared_ptr<CLASS>; \
-using UniquePtr = std::unique_ptr<CLASS>; \
 template<typename... Args>          \
 static auto createUnique(Args&&... args) { \
     return std::make_unique<CLASS>(args...); \
@@ -25,4 +23,6 @@ static auto createUnique(Args&&... args) { \
 template<typename... Args>          \
 static auto createShared(Args&&... args) { \
     return std::make_shared<CLASS>(args...); \
-}
+}                                          \
+using SharedPtr = std::shared_ptr<CLASS>; \
+using UniquePtr = std::unique_ptr<CLASS>
