dbc5c1696ce4af13b16beb5f6c4428333054969d
ptrNine <sockskilling@gmail.com>
2019-11-27 22:41:34 +0300

    Add DrawableManagerSP alias

--- src/graphics/DrawableManager.hpp
+++ src/graphics/DrawableManager.hpp
@@ -1,8 +1,10 @@
 #pragma once
 
+#include <memory>
 #include <SFML/Graphics/Drawable.hpp>
 
 #include "ObjectManager.hpp"
 
 using DrawableManager   = ObjectManager<sf::Drawable>;
 using TSDrawableManager = TSObjectManager<sf::Drawable>;
+using DrawableManagerSP = std::shared_ptr<DrawableManager>;
