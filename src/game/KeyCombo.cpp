c34d3c653db5dbaf8ebebb5371bf67b0f3cc038f
ptrNine <sockskilling@gmail.com>
2019-11-27 21:19:06 +0300

    Add KeyCombo detector

--- src/game/KeyCombo.cpp
+++ src/game/KeyCombo.cpp
@@ -1,4 +1,158 @@
-//
-// Created by slava on 10.11.19.
-//
+#include "KeyCombo.hpp"
 
+void ComboChecker::press(int keycode) {
+    // Double press prevent
+    for (auto key = _keys.rbegin(); key != _keys.rend(); ++key) {
+        if (key->keycode == keycode && key->state == State::Release)
+            break;
+        else if (key->keycode == keycode && key->state == State::Press)
+            return;
+    }
+
+    if (_keys.size() == _max_count)
+        _keys.pop_front();
+
+    _keys.emplace_back(KeyState{keycode, State::Press, timer().timestamp()});
+}
+
+void ComboChecker::release(int keycode) {
+    // Double release prevent
+    for (auto key = _keys.rbegin(); key != _keys.rend(); ++key) {
+        if (key->keycode == keycode && key->state == State::Press)
+            break;
+        else if (key->keycode == keycode && key->state == State::Release)
+            return;
+    }
+
+    if (_keys.size() == _max_count)
+        _keys.pop_front();
+
+    _keys.emplace_back(KeyState{keycode, State::Release, timer().timestamp()});
+}
+
+
+auto ComboChecker::test() -> std::optional<TestResult> {
+    std::vector<TestResult> results;
+
+    for (size_t combo_i = 0; combo_i < _combos.size(); ++combo_i) {
+        auto& combo = _combos[combo_i];
+
+        for (auto key = _keys.begin(); key != _keys.end(); ++key) {
+            bool hit = true;
+            auto real_key = key;
+
+            std::vector<decltype(_keys.begin())> ignored;
+
+            for (auto combo_key = combo.get().begin(); combo_key != combo.get().end(); ++combo_key, ++real_key) {
+                while (std::find(ignored.begin(), ignored.end(), real_key) != ignored.end())
+                    ++real_key;
+
+                if (real_key == _keys.end() || combo_key->keycode != real_key->keycode)
+                    goto fail;
+
+                switch (combo_key->press_type) {
+                    case KeyCombo::Type::Press: {
+                        if (real_key->state != State::Press)
+                            goto fail;
+
+                        if (combo_key != combo.get().end() - 1 && real_key != _keys.end() - 1) {
+                            double delta = ((real_key + 1)->timestamp - real_key->timestamp).sec();
+
+                            if (delta < combo_key->min_delay || delta > combo_key->max_delay)
+                                goto fail;
+                        }
+                    } break;
+
+
+                    case KeyCombo::Type::PressRelease: {
+                        if (real_key == _keys.end() - 1)
+                            goto fail;
+
+                        if (real_key->state != State::Press)
+                            goto fail;
+
+                        auto release_key = real_key + 1;
+                        for (; release_key != _keys.end(); ++release_key) {
+                            if (release_key->keycode == combo_key->keycode && release_key->state == State::Release) {
+                                ignored.push_back(release_key);
+                                break;
+                            }
+                        }
+
+                        if (release_key == _keys.end())
+                            goto fail;
+
+                        if (combo_key != combo.get().end() - 1 && real_key != _keys.end() - 2) {
+                            double delta = ((real_key + 2)->timestamp - real_key->timestamp).sec();
+
+                            if (delta < combo_key->min_delay || delta > combo_key->max_delay)
+                                goto fail;
+                        }
+
+                    } break;
+
+                    case KeyCombo::Type::Release: {
+                        if (real_key->state != State::Release)
+                            goto fail;
+
+                        if (combo_key != combo.get().end() - 1 && real_key != _keys.end() - 1) {
+                            double delta = ((real_key + 1)->timestamp - real_key->timestamp).sec();
+
+                            if (delta < combo_key->min_delay || delta > combo_key->max_delay)
+                                goto fail;
+                        }
+                    } break;
+                }
+
+                continue;
+
+                fail:
+                    hit = false;
+                    break;
+            }
+
+            if (hit) {
+                results.emplace_back(TestResult{combo.name(), combo_i});
+                break;
+            }
+        }
+    }
+
+    if (results.empty())
+        return std::nullopt;
+    else {
+        _keys.clear();
+
+        auto max = results.begin();
+
+        for (auto cur = results.begin() + 1; cur != results.end(); ++cur)
+            if (_combos[cur->combo_index].priority() > _combos[max->combo_index].priority())
+                max = cur;
+
+        return *max;
+    }
+}
+
+bool check_conflicts(const KeyCombo& l, const KeyCombo& r) {
+    auto found = std::search(l.get().rbegin(), l.get().rend(), r.get().rbegin(), r.get().rend(),
+            [](const KeyCombo::Key& l, const KeyCombo::Key& r) {
+                return  l.keycode == r.keycode &&
+                       (l.max_delay > r.min_delay && r.max_delay > l.min_delay);
+            });
+
+    if (found != l.get().rend() && found != l.get().rbegin())
+        fmt::print(stderr, "Warning! Combo '{}' intersects '{}'\n", r.name(), l.name());
+
+    return found != l.get().rend();
+}
+
+void ComboChecker::add_combo(const KeyCombo& combo)  {
+    for (auto& c : _combos) {
+        check_conflicts(c, combo);
+
+        if (check_conflicts(combo, c) && combo.get().size() == c.get().size())
+            fmt::print(stderr, "Warning! Combo '{}' hides '{}'\n", c.name(), combo.name());
+    }
+
+    _combos.push_back(combo);
+}
--- src/game/KeyCombo.hpp
+++ src/game/KeyCombo.hpp
@@ -1,8 +1,88 @@
-//
-// Created by slava on 10.11.19.
-//
+#pragma once
 
-#ifndef PLATFORMER_KEYCOMBO_HPP
-#define PLATFORMER_KEYCOMBO_HPP
+#include <string>
+#include <vector>
+#include <array>
+#include <tuple>
+#include <scl/scl.hpp>
+#include "../core/time.hpp"
 
-#endif //PLATFORMER_KEYCOMBO_HPP
+class KeyCombo {
+public:
+    enum class Type {
+        Press = 0, Release, PressRelease
+    };
+
+    struct Key {
+        int    keycode;
+        Type   press_type = Type::PressRelease;
+        double min_delay = 0;
+        double max_delay = 0.3;
+
+        Key(int ikeycode): keycode(ikeycode) {}
+        Key(int ikeycode, Type ipresstype): keycode(ikeycode), press_type(ipresstype) {}
+        Key(int ikeycode, Type ipresstype, double delay): keycode(ikeycode), press_type(ipresstype), max_delay(delay) {}
+        Key(int ikeycode, Type ipresstype, double imin_delay, double imax_delay):
+                keycode(ikeycode), press_type(ipresstype), min_delay(imin_delay), max_delay(imax_delay) {}
+        Key(int ikeycode, double delay): keycode(ikeycode), max_delay(delay) {}
+        Key(int ikeycode, double imin_delay, double imax_delay):
+                keycode(ikeycode), min_delay(imin_delay), max_delay(imax_delay) {}
+    };
+
+    KeyCombo(std::string name, std::vector<Key> keys): _name(std::move(name)), _vec(std::move(keys)) {}
+    KeyCombo(std::string name, size_t priority, std::vector<Key> keys):
+            _name(std::move(name)), _vec(std::move(keys)), _priority(priority) {}
+
+    void set(std::vector<Key> keys) {
+        _vec = std::move(keys);
+    }
+
+    size_t                  priority() const { return _priority; }
+    const std::vector<Key>& get     () const { return _vec; }
+    const std::string&      name    () const { return _name; }
+
+private:
+    size_t _priority = 0;
+    std::string _name;
+    std::vector<Key> _vec;
+};
+
+class ComboChecker {
+public:
+    DECLARE_SELF_FABRICS(ComboChecker);
+
+    enum class State {
+        Press, Release
+    };
+
+    struct KeyState {
+        int keycode;
+        State state = State::Press;
+        Timestamp timestamp;
+    };
+
+    struct KeyInfo {
+        int keycode;
+        State state;
+    };
+
+    struct TestResult {
+        std::string name;
+        size_t combo_index;
+    };
+
+    ComboChecker(size_t max_count): _max_count(max_count) {
+        _keys.reserve(_max_count);
+    }
+
+    void add_combo(const KeyCombo& combo);
+
+    void press  (int keycode);
+    void release(int keycode);
+    auto test() -> std::optional<TestResult>;
+
+private:
+    std::vector<KeyCombo> _combos;
+    scl::Ring<KeyState>   _keys;
+    size_t                _max_count;
+};
