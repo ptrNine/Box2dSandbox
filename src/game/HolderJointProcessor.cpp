32aca46529a4b49e57b77ea6709f7b4353e98fdd
ptrNine <sockskilling@gmail.com>
2019-12-11 02:24:44 +0300

    Create HolderJointProcessor

---
+++ src/game/HolderJointProcessor.cpp
@@ -0,0 +1,78 @@
+#include "HolderJointProcessor.hpp"
+
+#include <Box2D/Dynamics/Joints/b2RevoluteJoint.h>
+
+#include "../core/math.hpp"
+
+#include <iostream>
+
+HolderJointProcessor::~HolderJointProcessor() {
+    _joint->EnableMotor(false);
+}
+
+void HolderJointProcessor::update(const PhysicBodyBase&, double time_step)  {
+    float angle = std::clamp(_joint->GetJointAngle(), _joint->GetLowerLimit(), _joint->GetUpperLimit());
+
+    bool on_deadzone = math::float_eq(angle, _hold_angle, _deadzone_epsilon);
+
+    float speed = _joint->GetJointSpeed();
+
+    if (on_deadzone && speed > 0.f) {
+        speed -= time_step * _acceleration * _deadzone_acceleration_factor;
+        if (speed < 0)
+            speed = angle > _hold_angle ? - _max_speed * (angle - _hold_angle) : _max_speed * (_hold_angle - angle);
+    }
+    else if (on_deadzone && speed < 0.f) {
+        speed += time_step * _acceleration * _deadzone_acceleration_factor;
+        if (speed > 0)
+            speed = angle > _hold_angle ? - _max_speed * (angle - _hold_angle) : _max_speed * (_hold_angle - angle);
+    }
+    else {
+        if (angle > _hold_angle)
+            speed -= time_step * _acceleration;
+        else
+            speed += time_step * _acceleration;
+    }
+
+    speed = std::clamp(speed, -_max_speed, _max_speed);
+
+    _joint->EnableMotor(true);
+    _joint->SetMotorSpeed(speed);
+    _joint->SetMaxMotorTorque(_max_torque);
+}
+
+void HolderJointProcessor::set_hold_angle_if_valid(float angle) {
+    if (angle > _joint->GetLowerLimit() && angle < _joint->GetUpperLimit())
+        _hold_angle = angle;
+}
+
+void HolderJointProcessor::hold_angle(float angle) {
+    _hold_angle = std::clamp(angle, _joint->GetLowerLimit(), _joint->GetUpperLimit());
+}
+
+void HolderJointProcessor::Pressets::human_hand_weak_relaxed(HolderJointProcessor& hjp)  {
+    hjp.max_speed (13.f);
+    hjp.max_torque(1.3f);
+    hjp.acceleration(200.f);
+
+    hjp.deadzone_epsilon(0.25f);
+    hjp.deadzone_acceleration_factor(7.f);
+}
+
+void HolderJointProcessor::Pressets::human_hand_normal(HolderJointProcessor& hjp)  {
+    hjp.max_speed (14.f);
+    hjp.max_torque(2.6f);
+    hjp.acceleration(200.f);
+
+    hjp.deadzone_epsilon(0.20f);
+    hjp.deadzone_acceleration_factor(8.5f);
+}
+
+void HolderJointProcessor::Pressets::human_hand_fast_tense(HolderJointProcessor& hjp)  {
+    hjp.max_speed (15.f);
+    hjp.max_torque(4.2f);
+    hjp.acceleration(140.f);
+
+    hjp.deadzone_epsilon(0.15f);
+    hjp.deadzone_acceleration_factor(10.f);
+}
---
+++ src/game/HolderJointProcessor.hpp
@@ -0,0 +1,80 @@
+#pragma once
+
+#include "JointProcessor.hpp"
+#include "../core/helper_macros.hpp"
+
+class HolderJointProcessor : public JointProcessor {
+public:
+    struct Pressets {
+        static void human_hand_weak_relaxed(HolderJointProcessor& hjp);
+        static void human_hand_normal      (HolderJointProcessor& hjp);
+        static void human_hand_fast_tense  (HolderJointProcessor& hjp);
+    };
+
+public:
+    DECLARE_SELF_FABRICS(HolderJointProcessor);
+
+    HolderJointProcessor(class b2RevoluteJoint* j, float hold_angle = 0.0f):
+            _joint(j),
+            _hold_angle(hold_angle)
+    {}
+
+    ~HolderJointProcessor() final;
+    void update(const class PhysicBodyBase&, double time_step) final;
+
+private:
+    enum class overflight : uint8_t { off, upper, lower };
+
+    std::function<float(float)> _motion_function = MotionFunction::linear;
+
+    b2RevoluteJoint* _joint;
+
+    float _hold_angle = 0.f;
+    float _last_hold_angle = 0.f;
+    float _valid_hold_angle_epsilon = 0.01f;
+
+    float _min_speed    = 1.f;
+    float _max_speed    = 15.f;
+    float _max_torque   = 8.f;
+    float _acceleration = 80.f;
+
+    float _deadzone_epsilon = 0.15f;
+    float _deadzone_acceleration_factor = 20.f;
+public:
+    /// Sets motion function
+    DECLARE_SET(motion_function);
+
+    /// Sets hold angle if it in the lower/upper range
+    void set_hold_angle_if_valid(float angle);
+
+    /// Sets hold angle, clamped to lower/upper range
+    void hold_angle(float angle);
+
+    /// Returns hold angle
+    DECLARE_GET(hold_angle);
+
+    /// Returns max angle speed
+    DECLARE_GET(max_speed);
+    /// Sets max angle speed
+    DECLARE_SET(max_speed);
+
+    /// Returns max torque
+    DECLARE_GET(max_torque);
+    /// Sets max torque
+    DECLARE_SET(max_torque);
+
+    /// Returns angle speed acceleration
+    DECLARE_GET(acceleration);
+    /// Sets angle speed acceleration
+    DECLARE_SET(acceleration);
+
+    /// Returns angle epsilon of "deadzone"
+    DECLARE_GET(deadzone_epsilon);
+    /// Sets angle epsilon of "deadzone"
+    DECLARE_SET(deadzone_epsilon);
+
+    /// Returns angle speed acceleration in "deadzone"
+    DECLARE_GET(deadzone_acceleration_factor);
+    /// Sets angle speed acceleration in "deadzone"
+    DECLARE_SET(deadzone_acceleration_factor);
+};
