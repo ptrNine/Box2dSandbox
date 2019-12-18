de422d7d38dc1c3194ebdbb08dc5cc90b1d0683f
ptrNine <sockskilling@gmail.com>
2019-12-19 02:58:28 +0300

    game/MotionInterfaces: refactor all

--- src/game/MotionInterfaces.cpp
+++ src/game/MotionInterfaces.cpp
@@ -1,146 +1,369 @@
 #include "MotionInterfaces.hpp"
 
+#include <Box2D/Dynamics/Joints/b2RevoluteJoint.h>
 #include <scl/scl.hpp>
 
 #include "PhysicHumanBody.hpp"
 #include "HolderJointProcessor.hpp"
 #include "../core/math.hpp"
 
-
-// Periodic counter
-void MotionInterfaces::create_periodic_counter(PhysicBodyBase& body, const std::string& name, double period, double start) {
-    body.user_data()[name + "_period"] = period;
-    body.user_data()[name + "_acc"] = start;
-    body.user_data()[name + "_used_by"] = size_t(0);
-}
-
-void MotionInterfaces::delete_periodic_counter(PhysicBodyBase& body, const std::string& name) {
-    if (!body.user_data().has(name + "_used_by")) {
-        // Todo: log warning
-        return;
-    }
-    if (body.user_data().cast<size_t>(name + "_used_by") != 0) {
-        // Todo: log warning
-        return;
+namespace motion_interface {
+    PeriodicCounter::PeriodicCounter(const std::weak_ptr<PhysicBodyBase>& body, StrCRefT name): _name(name) {
+        add_binding(body);
     }
 
-    body.user_data().erase(name + "_period");
-    body.user_data().erase(name + "_acc");
-    body.user_data().erase(name + "_used_by");
-}
+    void PeriodicCounter::add_binding(const std::weak_ptr<PhysicBodyBase>& body) {
+        _bodies.emplace_back(body);
+        update_bindings();
+    }
 
-
-
-auto operated_joints = scl::Array{
-    PhysicHumanBody::BodyJoint_Chest_ThighL,
-    PhysicHumanBody::BodyJoint_Chest_ThighR,
-    PhysicHumanBody::BodyJoint_ThighL_ShinL,
-    PhysicHumanBody::BodyJoint_ThighR_ShinR
-};
-
-void MotionInterfaces::HumanRun::apply(PhysicHumanBody& human) {
-    if (!is_exists(human)) {
-        using BJ = PhysicHumanBody::BodyJoint;
-        auto joint_processors = scl::Vector<HolderJointProcessor::WeakPtr>(operated_joints.size());
-
-
-        for (size_t i = 0; i < joint_processors.size(); ++i) {
-            joint_processors[i] = human.joint_processor_new<HolderJointProcessor>(
-                    std::string("Motion::") + PhysicHumanBody::joint_names[operated_joints[i]].data(),
-                    operated_joints[i], 0);
-            HolderJointProcessor::Pressets::human_leg_fast_tense(*joint_processors[i].lock());
+    void PeriodicCounter::set(double period, double start) const {
+        for (auto& b : _bodies) {
+            if (auto body = b.lock()) {
+                auto& ud = body->user_data();
+                ud.cast<double>(_n_period) = period;
+                ud.cast<double>(_n_acc) = std::clamp(start, 0.0, period);
+                ud.cast<double>(_n_factor) = std::clamp(start, 0.0, period) / period;
+            }
         }
+    }
 
-        joint_processors[0].lock()->hold_angle(0.8f);
-        joint_processors[1].lock()->hold_angle(-0.8f);
+    void PeriodicCounter::set_period(double period) const {
+        for (auto& b : _bodies) {
+            if (auto body = b.lock()) {
+                auto& ud = body->user_data();
+                auto& p = ud.cast<double>(_n_period);
+                auto& a = ud.cast<double>(_n_acc);
 
-        human.user_data()["_leg_time_period"] = 0.6;
-        human.user_data()["_leg_time_acc"] = 0.0;
-        human.user_data()["_leg_upper_limit"] = 0.8f;
-        human.user_data()["_leg_lower_limit"] = -0.8f;
-        human.user_data()["_leg_enable_interpolation"] = false;
+                a = ud.cast<double>(_n_factor) * period;
+                p = period;
+            }
+        }
+    }
 
+    void PeriodicCounter::set_start(double start) const {
+        for (auto& b : _bodies) {
+            if (auto body = b.lock()) {
+                auto& p = body->user_data().cast<double>(_n_period);
+                body->user_data().cast<double>(_n_acc) = std::clamp(start, 0.0, p);
+            }
+        }
+    }
 
-        human.add_update("Motion::update", [joint_processors] (PhysicBodyBase& body, double timestep) {
-            auto& human = static_cast<PhysicHumanBody&>(body);
+    void PeriodicCounter::destroy() const {
+        for (auto& b : _bodies) {
+            if (auto body = b.lock()) {
+                auto& ud = body->user_data();
+                auto used_by = ud.cast<size_t>(_n_used_by);
 
-            auto locked = joint_processors.map(
-                    [](const HolderJointProcessor::WeakPtr& w) { return w.lock(); });
-            auto all_locked = locked.reduce(
-                    [](bool r, const HolderJointProcessor::SharedPtr& s) { return r && s; }, true);
-
-            if (all_locked) {
-                HMAP_GET(double&, _leg_time_acc,    human.user_data());
-                HMAP_GET(double&, _leg_time_period, human.user_data());
-                HMAP_GET(float,   _leg_upper_limit, human.user_data());
-                HMAP_GET(float,   _leg_lower_limit, human.user_data());
-                HMAP_GET(bool,    _leg_enable_interpolation, human.user_data());
-
-                if (_leg_enable_interpolation) {
-                    _leg_time_acc = fmod(_leg_time_acc + timestep, _leg_time_period);
-
-                    auto factor = std::fabs(2.f * float(math::inverse_lerp(0.0, _leg_time_period, _leg_time_acc)) - 1.f);
-
-                    locked[0]->hold_angle(
-                            math::lerp(_leg_lower_limit, _leg_upper_limit, factor));
-                    locked[1]->hold_angle(
-                            math::lerp(_leg_lower_limit, _leg_upper_limit, 1.f - factor));
-                }
-                else {
-                    _leg_time_acc += timestep;
-                    if (_leg_time_acc > _leg_time_period / 2.f) {
-                        _leg_time_acc = fmod(_leg_time_acc, _leg_time_period / 2.f);
-
-                        float tmp = locked[0]->hold_angle();
-                        locked[0]->hold_angle(locked[1]->hold_angle());
-                        locked[1]->hold_angle(tmp);
-                    }
+                if (used_by == 0) {
+                    ud.erase(_n_factor);
+                    ud.erase(_n_acc);
+                    ud.erase(_n_period);
+                    ud.erase(_n_used_by);
+                    body->remove_update(_n_update);
+                } else {
+                    // Todo: log warning
                 }
             }
+        }
+    }
+
+    struct PeriodicCounterFunctor {
+        void operator()(PhysicBodyBase& body, double timestep) const {
+            auto& acc    = body.user_data().cast<double>(_n_acc);
+            auto& period = body.user_data().cast<double>(_n_period);
+
+            acc = fmod(acc + timestep, period);
+            body.user_data().cast<double>(_n_factor) = acc / period;
+        }
+
+        StrT _n_acc, _n_period, _n_factor;
+    };
+
+    void PeriodicCounter::update_bindings() {
+        std::string pref = std::string("__") + _name;
+        _n_acc     = pref + "_acc";
+        _n_factor  = pref + "_factor";
+        _n_update  = pref + "_update";
+        _n_used_by = pref + "_used_by";
+        _n_period  = pref + "_period";
+
+        double last_valid_period = 1.0;
+
+        for (auto& b : _bodies) {
+            if (auto body = b.lock()) {
+                if (!body->is_update_exists(_n_update)) {
+                    auto& ud = body->user_data();
+
+                    ud[_n_acc]     = 0.0;
+                    ud[_n_factor]  = 0.0;
+                    ud[_n_period]  = last_valid_period;
+                    ud[_n_used_by] = size_t(0);
+
+                    body->add_update(_n_update, PeriodicCounterFunctor{_n_acc, _n_period, _n_factor});
+                } else {
+                    last_valid_period = body->user_data().cast<double>(_n_period);
+                }
+            }
+        }
+    }
+
+
+    AnimatedJoint::AnimatedJoint(const std::weak_ptr<BodyWithJoints>& body, StrCRefT name, int joint_index, StrCRefT counter_name): _name(name) {
+        add_binding(body, joint_index, counter_name);
+    }
+
+    void AnimatedJoint::add_binding(const std::weak_ptr<BodyWithJoints>& body, int joint_index, StrCRefT counter_name) {
+        _bindings.push_back({body, joint_index, counter_name});
+        update_bindings();
+    }
+
+    const AnimatedJoint& AnimatedJoint::set_frames(VecCRefT<Frame> _frames) const {
+        auto frames = _frames;
+        for (auto& f : frames)
+            f.start_point = std::clamp(f.start_point, 0.0, 1.0);
+
+        for (auto& b : _bindings) {
+            if (auto body = b.body.lock()) {
+                auto& ud = body->user_data();
+
+                auto& frames_count = ud.cast<size_t>(_n_frames_count);
+
+                // Remove unused frames
+                if (frames.size() < frames_count) {
+                    for (size_t i = frames.size(); i < frames_count; ++i) {
+                        auto p = _n_frame_pref + std::to_string(i);
+                        ud.erase(p + "_start");
+                        ud.erase(p + "_angle");
+                    }
+                }
+
+                frames_count = frames.size();
+
+                // Setup frames
+                for (size_t i = 0; i < frames.size(); ++i) {
+                    auto p = _n_frame_pref + std::to_string(i);
+                    ud[p + "_start"] = frames[i].start_point;
+                    ud[p + "_angle"] = frames[i].target_angle;
+                }
+            }
+            else {
+                // Todo: log warning
+            }
+        }
+
+        return *this;
+    }
+    const AnimatedJoint& AnimatedJoint::set_shift(double time_shift) const {
+        for (auto& b : _bindings) {
+            if (auto body = b.body.lock()) {
+                auto& ud = body->user_data();
+                body->user_data().cast<double>(_n_shift) = time_shift;
+            }
+            else {
+                // Todo: log warning
+            }
+        }
+
+        return *this;
+    }
+
+    void AnimatedJoint::destroy() const {
+        for (auto& b : _bindings) {
+            if (auto body = b.body.lock()) {
+                auto& ud = body->user_data();
+
+                auto frames_count = ud.cast<size_t>(_n_frames_count);
+                for (size_t i = 0; i < frames_count; ++i) {
+                    auto pref = _n_frame_pref + std::to_string(i);
+                    ud.erase(pref + "_start");
+                    ud.erase(pref + "_angle");
+                }
+
+                body->remove_update(_n_update);
+                body->remove_joint_processor(_n_joint_processor);
+                ud.erase(_n_frames_count);
+                ud.erase(_n_last_frame);
+                ud.erase(_n_shift);
+            }
+            else {
+                // Todo: log warning
+            }
+        }
+    }
+
+    struct AnimatedJointFunctor {
+        void operator()(PhysicBodyBase& _body, double) const {
+            auto& body = static_cast<BodyWithJoints&>(_body);
+
+            auto& ud = body.user_data();
+            auto counter_name = ud.cast<StrT>(_n_counter);
+            auto factor       = fmod(ud.cast<double>("__" + counter_name + "_factor") + ud.cast<double>(_n_shift), 1.0);
+            auto frames_count = ud.cast<size_t>(_n_frames_count);
+            auto& last_frame  = ud.cast<size_t>(_n_last_frame);
+            auto hjp          = body.joint_processor_cast_get<HolderJointProcessor>(_n_joint_processor).lock();
+
+            if (!hjp) {
+                // Todo: assert
+            }
+
+            auto last_frame_start = ud.cast<double>(_n_frame_pref + std::to_string(last_frame) + "_start");
+            size_t found_idx = last_frame;
+
+            if (factor < last_frame_start)
+                found_idx = 0;
+
+            for (; found_idx != frames_count - 1; ++found_idx) {
+                auto start = ud.cast<double>(_n_frame_pref + std::to_string(found_idx + 1) + "_start");
+
+                if (factor < start)
+                    break;
+            }
+
+            last_frame = found_idx;
+            auto angle = ud.cast<float>(_n_frame_pref + std::to_string(last_frame) + "_angle");
+
+            DEBUG_VAL_LMAO(angle);
+
+            hjp->hold_angle(angle);
+        }
+
+        StrT _n_shift, _n_frame_pref, _n_last_frame, _n_frames_count, _n_joint_processor, _n_counter;
+    };
+
+    void AnimatedJoint::update_bindings() {
+        _n_shift           = "__" + _name + "_shift";
+        _n_frame_pref      = "__" + _name + "_frame";
+        _n_last_frame      = "__" + _name + "_last_frame";
+        _n_frames_count    = "__" + _name + "_frames_count";
+        _n_joint_processor = "__" + _name + "_joint_processor";
+        _n_counter         = "__" + _name + "_counter_name";
+        _n_update          = "__" + _name + "_update";
+
+        size_t last_valid_index = std::numeric_limits<size_t>::max();
+
+        for (size_t ib = 0; ib < _bindings.size(); ++ib) {
+            auto& b = _bindings[ib];
+
+            if (auto body = b.body.lock()) {
+                if (!body->is_joint_processor_exists(_n_joint_processor)) {
+                    auto& ud = body->user_data();
+
+                    ud[_n_shift]      = 0.0;
+                    ud[_n_last_frame] = size_t(0);
+                    ud[_n_counter]    = b.counter_name;
+
+                    if (b.joint_index == -1) {
+                        // Todo: assert
+                    }
+
+                    body->joint_processor_new<HolderJointProcessor>(_n_joint_processor, b.joint_index);
+                    body->add_update(_n_update, AnimatedJointFunctor{
+                            _n_shift, _n_frame_pref, _n_last_frame, _n_frames_count, _n_joint_processor, _n_counter
+                    });
+
+                    if (last_valid_index != std::numeric_limits<size_t>::max()) {
+                        // Todo: set frames
+                    } else
+                        ud[_n_frames_count] = size_t(0);
+
+
+                } else
+                    last_valid_index = ib;
+            }
+        }
+    }
+} // namespace motion_interface
+
+
+
+void MotionInterface::Human::create_ticker_joint(
+        PhysicHumanBody& human,
+        int body_joint,
+        StrCRefT name,
+        StrCRefT counter_name)
+{
+    if (!ticker_joint_is_exists(human, name)) {
+        using BJ = PhysicHumanBody::BodyJoint;
+
+        auto hjp = human.joint_processor_new<HolderJointProcessor>(name, BJ(body_joint));
+
+        human.user_data()[name + "_upper_limit"] = PhysicHumanBody::joints_angle_limits->max;
+        human.user_data()[name + "_lower_limit"] = PhysicHumanBody::joints_angle_limits->min;
+        human.user_data()[name + "_last_time"] = human.user_data().cast<double>(std::string("__") + counter_name + "_acc");
+
+        HolderJointProcessor::Pressets::human_leg_fast_tense(*hjp.lock());
+        hjp.lock()->hold_angle(PhysicHumanBody::joints_angle_limits->max);
+
+        human.add_update(name + "_update", [counter_name, name](PhysicBodyBase& body, double timestep) {
+            auto& human = static_cast<PhysicHumanBody&>(body);
+            auto& hjp   = *human.joint_processor_cast_get<HolderJointProcessor>(name).lock();
+
+            auto hold  = hjp.hold_angle();
+            auto lower = human.user_data().cast<float>(name + "_lower_limit");
+            auto upper = human.user_data().cast<float>(name + "_upper_limit");
+            auto& time  = human.user_data().cast<double>(name + "_last_time");
+            auto current_time = human.user_data().cast<double>(std::string("__") + counter_name + "_acc");
+
+            if (current_time < time)
+                hjp.hold_angle(hold == upper ? lower : upper);
+
+            time = current_time;
         });
-    } else {
+
+
+    }
+    else {
         // Todo: log warning
     }
 }
 
-void MotionInterfaces::HumanRun::set_run_period(PhysicHumanBody& human, double time) {
-    human.user_data()["_leg_time_period"] = time;
-}
-
-void MotionInterfaces::HumanRun::set_run_state(PhysicHumanBody& human, float state) {
-    human.user_data()["_leg_time_acc"] =
-            human.user_data().cast<double>("_leg_time_period") * std::clamp(state, 0.f, 1.f);
-}
-
-void MotionInterfaces::HumanRun::set_thigh_limits(class PhysicHumanBody& human, float lower, float upper) {
-    human.user_data()["_leg_upper_limit"] = math::angle::constraint(lower);
-    human.user_data()["_leg_lower_limit"] = math::angle::constraint(upper);
-}
-
-void MotionInterfaces::HumanRun::destroy(PhysicHumanBody& human) {
-    if (is_exists(human)) {
-        for (auto j : operated_joints)
-            human.remove_joint_processor(std::string("Motion::") + PhysicHumanBody::joint_names[j].data());
-
-        human.remove_update("Motion::update");
-        human.user_data().erase("_leg_time_period");
-        human.user_data().erase("_leg_time_acc");
-        human.user_data().erase("_leg_upper_limit");
-        human.user_data().erase("_leg_lower_limit");
-    } else {
+void MotionInterface::Human::remove_ticker_joint(PhysicHumanBody& human, StrCRefT name) {
+    if (ticker_joint_is_exists(human, name)) {
+        human.remove_update(name + "_update");
+        human.user_data().erase(name + "_upper_limit");
+        human.user_data().erase(name + "_lower_limit");
+        human.user_data().erase(name + "_last_time");
+    }
+    else {
         // Todo: log warning
     }
 }
 
-bool MotionInterfaces::HumanRun::is_exists(const PhysicHumanBody& human) {
-    bool res = human.is_update_exists("Motion::update") &&
-               human.user_data().has("_leg_time_period") &&
-               human.user_data().has("_leg_time_acc"   ) &&
-               human.user_data().has("_leg_upper_limit") &&
-               human.user_data().has("_leg_lower_limit");
+void MotionInterface::Human::ticker_joint_change_dir(PhysicHumanBody& human, StrCRefT name) {
+    if (ticker_joint_is_exists(human, name)) {
+        auto& hjp = *human.joint_processor_cast_get<HolderJointProcessor>(name).lock();
+        auto hold  = hjp.hold_angle();
+        auto lower = human.user_data().cast<float>(name + "_lower_limit");
+        auto upper = human.user_data().cast<float>(name + "_upper_limit");
 
-    for (auto j : operated_joints)
-        res = res && human.is_joint_processor_exists(std::string("Motion::") + PhysicHumanBody::joint_names[j].data());
+        hjp.hold_angle(hold == upper ? lower : upper);
+    }
+    else {
+        // Todo: log warning
+    }
+}
 
-    return res;
+void MotionInterface::Human::ticker_joint_set_constraint(PhysicHumanBody& human, StrCRefT name, float min, float max) {
+    if (ticker_joint_is_exists(human, name)) {
+        auto& hjp = *human.joint_processor_cast_get<HolderJointProcessor>(name).lock();
+        auto  hold  = hjp.hold_angle();
+        auto& lower = human.user_data().cast<float>(name + "_lower_limit");
+        auto& upper = human.user_data().cast<float>(name + "_upper_limit");
+
+        auto old_upper = upper;
+
+        lower = min;
+        upper = max;
+
+        hjp.hold_angle(hold == old_upper ? lower : upper);
+    }
+    else {
+        // Todo: log warning
+    }
+}
+
+bool MotionInterface::Human::ticker_joint_is_exists(PhysicHumanBody& human, StrCRefT name) {
+    return human.is_joint_processor_exists(name);
 }
--- src/game/MotionInterfaces.hpp
+++ src/game/MotionInterfaces.hpp
@@ -1,25 +1,146 @@
 #pragma once
 
 #include <string>
+#include <vector>
+#include <memory>
+#include <optional>
+#include "../core/helper_macros.hpp"
 
 class PhysicBodyBase;
 class PhysicHumanBody;
+class BodyWithJoints;
 
-struct MotionInterfaces {
-    static void create_periodic_counter(
-            PhysicBodyBase& body,
-            const std::string& name,
-            double period,
-            double start = 0.0);
+namespace motion_interface {
+    using StrT     = std::string;
+    using StrRefT  = StrT&;
+    using StrCRefT = const StrT&;
 
-    static void delete_periodic_counter(PhysicBodyBase& body, const std::string& name);
+    template <typename T>
+    using VecT     = std::vector<T>;
+    template <typename T>
+    using VecRefT  = VecT<T>&;
+    template <typename T>
+    using VecCRefT = const VecT<T>&;
 
-    struct HumanRun {
-        static void apply           (PhysicHumanBody& human);
-        static void destroy         (PhysicHumanBody& human);
-        static bool is_exists       (const PhysicHumanBody& human);
-        static void set_run_period  (PhysicHumanBody& human, double time);
-        static void set_run_state   (PhysicHumanBody& human, float state);
-        static void set_thigh_limits(PhysicHumanBody& human, float lower, float upper);
+
+    class PeriodicCounter {
+    public:
+        PeriodicCounter (const std::weak_ptr<PhysicBodyBase>& body, StrCRefT name);
+
+        /// Bind to body
+        void add_binding(const std::weak_ptr<PhysicBodyBase>& body);
+
+        /// Bind to bodies
+        template <typename WeakPtr>
+        void add_binding(VecCRefT<WeakPtr> bodies) {
+            for (auto& p : bodies)
+                add_binding(p);
+        }
+
+        /// Set settings
+        void set       (double period, double start) const;
+        void set_start (double start = 0.f) const;
+        void set_period(double period) const;
+
+        /// Destroy all resources from bodies
+        void destroy() const;
+
+    protected:
+        void update_bindings();
+
+        VecT<std::weak_ptr<PhysicBodyBase>> _bodies;
+        StrT _name;
+
+        StrT _n_period;
+        StrT _n_acc;
+        StrT _n_used_by;
+        StrT _n_update;
+        StrT _n_factor;
+
+    public:
+        DECLARE_GET_SET(name);
+    };
+
+
+    class AnimatedJoint {
+    public:
+        struct Frame {
+            double start_point;
+            float  target_angle;
+        };
+
+        struct Binding {
+            std::weak_ptr<BodyWithJoints> body;
+            int joint_index;
+            StrT counter_name;
+        };
+
+        AnimatedJoint(const std::weak_ptr<BodyWithJoints>& body, StrCRefT name, int joint_index = -1, StrCRefT counter_name = "");
+
+        /// Bind to body
+        void add_binding(const std::weak_ptr<BodyWithJoints>& body, int joint_index = -1, StrCRefT counter_name = "");
+
+        /// Bind to bodies
+        template <typename WeakPtr>
+        void add_binding(VecCRefT<WeakPtr> bodies) {
+            for (auto& p : bodies)
+                add_binding(p);
+        }
+
+        const AnimatedJoint& set_frames(VecCRefT<Frame> frames) const;
+        const AnimatedJoint& set_shift (double time_shift) const;
+
+        void destroy() const;
+
+    protected:
+        void update_bindings();
+
+        VecT<Binding> _bindings;
+        StrT _name;
+
+        StrT _n_frame_pref;
+        StrT _n_last_frame;
+        StrT _n_frames_count;
+        StrT _n_shift;
+        StrT _n_joint_processor;
+        StrT _n_counter;
+        StrT _n_update;
+
+    public:
+        DECLARE_GET(n_joint_processor);
+    };
+} // namespace motion_interface
+
+
+
+struct MotionInterface {
+    using StrT     = std::string;
+    using StrRefT  = StrT&;
+    using StrCRefT = const StrT&;
+
+    template <typename T>
+    using VecT     = std::vector<T>;
+    template <typename T>
+    using VecRefT  = VecT<T>&;
+    template <typename T>
+    using VecCRefT = const VecT<T>&;
+
+    struct Human {
+        static void create_ticker_joint        (PhysicHumanBody& human, int body_joint, StrCRefT name, StrCRefT counter_name);
+        static void remove_ticker_joint        (PhysicHumanBody& human, StrCRefT name);
+        static bool ticker_joint_is_exists     (PhysicHumanBody& human, StrCRefT name);
+        static void ticker_joint_change_dir    (PhysicHumanBody& human, StrCRefT name);
+        static void ticker_joint_set_constraint(PhysicHumanBody& human, StrCRefT name, float min, float max);
+
+
+        struct AnimationFrame {
+            double start_time   = 0.0;
+            float  target_angle = 0.f;
+        };
+
+        static void create_animated_joint    (PhysicHumanBody& human, int body_joint, StrCRefT name, StrCRefT counter_name);
+        static void remove_animated_joint    (PhysicHumanBody& human, StrCRefT name);
+        static bool animated_joint_is_exists (PhysicHumanBody& human, StrCRefT name);
+        static void animated_joint_set_frames(PhysicHumanBody& human, StrCRefT name, VecCRefT<AnimationFrame> frames);
     };
 };
